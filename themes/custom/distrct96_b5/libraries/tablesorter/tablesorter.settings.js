4efed7c27ecc75a560d89872d3087c3a4ed2fc47
Rob Evans <mysticbovine@gmail.com>
2023-06-24 17:00:59 -0700

    Table sorting added to a block.

--- themes/custom/distrct96_b5/libraries/tablesorter/tablesorter.settings.js
+++ themes/custom/distrct96_b5/libraries/tablesorter/tablesorter.settings.js
@@ -98,6 +98,60 @@
             }
             
           });
+          $("#cot").tablesorter({
+            // this will apply the bootstrap theme if "uitheme" widget is included
+            // the widgetOptions.uitheme is no longer required to be set
+            theme : "bootstrap",
+
+            sortList: [[1,0],[0,0]],
+        
+            widthFixed: true,
+        
+            headerTemplate : '{content} {icon}', // new in v2.7. Needed to add the bootstrap icon!
+
+            headers: {
+                // disable sorting of the first & second column - before we would have to had made two entries
+                // note that "first-name" is a class on the span INSIDE the first column th cell
+                '.special-presentation, .registration, .zoom, .password' : {
+                  // disable it by setting the property sorter to false
+                  sorter: false
+                }
+            },
+            // widget code contained in the jquery.tablesorter.widgets.js file
+            // use the zebra stripe widget if you plan on hiding any rows (filter widget)
+            widgets : [ "uitheme", "filter", "columns", "zebra" ],
+        
+            widgetOptions : {
+              // using the default zebra striping class name, so it actually isn't included in the theme variable above
+              // this is ONLY needed for bootstrap theming if you are using the filter widget, because rows are hidden
+              zebra : ["even", "odd"],
+        
+              // class names added to columns when sorted
+              columns: [ "primary", "secondary", "tertiary" ],
+        
+              // reset filters button
+              filter_reset : ".reset",
+        
+              // extra css class name (string or array) added to the filter element (input or select)
+              filter_cssFilter: "form-control",
+        
+              // set the uitheme widget to use the bootstrap theme class names
+              // this is no longer required, if theme is set
+              // ,uitheme : "bootstrap"
+              filter_functions : {
+
+                // Add select menu to this column
+                // set the column value to true, and/or add "filter-select" class name to header
+                '.zone' : true, 
+                '.host' : true,
+                '.day'  : true,
+              
+              }
+           
+        
+            }
+            
+          });
           $("#onlineMeetings").tablesorter({
             // this will apply the bootstrap theme if "uitheme" widget is included
             // the widgetOptions.uitheme is no longer required to be set
